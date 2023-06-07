e38f138d88405ffe05f8488e1c6b534026c9de00
Kagev <kaplitskiygv@gmail.com>
2023-06-07 10:39:18 +0300

    update readme and setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 setup(
     name='Willy',
-    version='v0.9.3',
-    description='Твій особистий помічник',
+    version='v0.9.7',
+    description='Personal Assistant Willy',
     url='https://github.com/Kagev/GOIT_PersonalAssistance',
     author='PyCrafters',
     author_email='PyCrafters@goit.com',
-    license='MIT',
+    license='GNU',
     packages=find_namespace_packages(),
-    entry_points={'willy': ['willy = project_willy.main']}
+    classifiers=["Programming Language :: Python :: 3",
+                "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                "Operating System :: OS Independent",
+                ],
+    entry_points={'willy': ['willy = project_willy.main']
+                  }
 )
