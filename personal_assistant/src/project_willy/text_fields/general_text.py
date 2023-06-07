3000fbb816ad6c358f5843be479c73f915b9f732
Kagev <kaplitskiygv@gmail.com>
2023-06-07 23:36:10 +0300

    add writes for autotake name and version from METADATA

--- personal_assistant/src/project_willy/text_fields/general_text.py
+++ personal_assistant/src/project_willy/text_fields/general_text.py
@@ -1,9 +1,17 @@
+from personal_assistant.src import setup
+import re
+import ast
+import pathlib
+import pkg_resources
 
+# Take name and version from METADATA
+version = pkg_resources.get_distribution('willy').version
+name = pkg_resources.get_distribution('willy').project_name
 class GeneralText:
 
     start_message = \
     '\n{:^40}\n{:^40}\n'.format('---HELLO---', '-'*40)+\
-    '|{:^38}|\n'.format("I'M WILLY ASSISTANT v0.9.*")+\
+    '|{:^38}|\n'.format(f"I'M {name} ASSISTANT {version}")+\
     '|{:^38}|\n{:^40}\n'.format("NICE TO MEET YOU!", '-'*40)
     continue_input_message = 'Press enter to continue.\n>>> '
     wrong_input_message = '\nHmm.. Somesing wrong. Try again.\n'
