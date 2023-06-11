522dd0209a310859d3ecc4388208b152d71e06c4
Kagev <kaplitskiygv@gmail.com>
2023-06-11 15:29:08 +0300

    add authorization and registration files

--- personal_assistant/src/project_willy/methods/registration.py
+++ personal_assistant/src/project_willy/methods/registration.py
@@ -4,48 +4,66 @@ import json
 import getpass
 
 
-class Authorization:
-
+class Registration:
     def __init__(self):
-        self.key = None
         self.password = None
         self.username = None
         self.salt = None
-        self.users = {}
+        self.users = []
 
     def load_user(self):
         try:
             with open("users.json", "r") as file:
-                for line in file:
-                    user_data = json.loads(line)
-                    if 'username' in user_data:
-                        self.users[user_data['username']] = user_data
+                self.users = json.load(file)
+                if not isinstance(self.users, list):
+                    self.users = []  # Initialize as an empty list if the loaded data is not a list
         except (FileNotFoundError, json.JSONDecodeError):
-            self.users = {}
+            self.users = []
 
-    def login(self):
+    def save_user(self):
+        with open("users.json", "a") as file:
+            user_data = {
+                'username': self.username,
+                'salt': self.salt.hex(),
+                'key': self.key.hex()
+            }
+            json.dump(user_data, file)
+            file.write('\n')
+
+    def registration_user(self):
+        self.load_user()
+
         print("Enter your login name")
         self.username = input(">>>: ")
+        print("Enter your password")
+        self.password = getpass.getpass(prompt='Password: ', stream=None)
+        print("Enter your pass for confirm ")
+        confirm_password = getpass.getpass(prompt='Password: ', stream=None)
 
-        if self.username not in self.users:
-            print("User does not exist")
+        if self.password != confirm_password:
+            print("Your passwords do not match!")
             return
-        else:
-            print("Enter your password")
-            self.password = getpass.getpass(">>>: ")
 
-        user_data = self.users[self.username]
-        self.salt = bytes.fromhex(user_data['salt'])
-        self.key = bytes.fromhex(user_data['key'])
-        new_key = hashlib.pbkdf2_hmac('sha256', self.password.encode('utf-8'), self.salt, 100000)
+        for user in self.users:
+            if user['username'] == self.username:
+                print("User already exists")
+                return
 
-        if self.key == new_key:
-            print('Login successful')
-        else:
-            print('Invalid Login')
+        self.salt = os.urandom(32)
+        self.key = hashlib.pbkdf2_hmac('sha256', self.password.encode('utf-8'), self.salt, 100000)
+
+        user_data = {
+            'username': self.username,
+            'salt': self.salt.hex(),
+            'key': self.key.hex()
+        }
+
+        self.users.append(user_data)
+        self.save_user()
+
+        print("Registration successful.")
 
 
 if __name__ == "__main__":
-    authorization = Authorization()
-    authorization.load_user()
-    authorization.login()
+    registration = Registration()
+    registration.registration_user()
