c96528f481ce2266a0fc64ac7a05f54947f6840b
Kagev <kaplitskiygv@gmail.com>
2023-06-15 14:08:20 +0300

    text fo auth menu

--- personal_assistant/src/project_willy/text_fields/auth_menu_text.py
+++ personal_assistant/src/project_willy/text_fields/auth_menu_text.py
@@ -2,7 +2,7 @@
 class AuthMenuText:
 
     auth_message =\
-    '\n{:^40}\n{:^40}\n'.format('---MAIN MENU---', '-'*40)+\
+    '\n{:^40}\n{:^40}\n'.format('---MENU SING IN---', '-'*40)+\
     '|{:^38}|\n{:^40}\n'.format('1. REGISTRATION', '-'*40)+\
     '|{:^38}|\n{:^40}\n'.format('2. AUTHORIZATION', '-'*40)+\
     '|{:^38}|\n{:^40}\n'.format('EXIT', '-'*40)
